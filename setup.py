b72ca70c4caca9a29c91b1f2375e885fb348bb51
Marc Brinkmann <git@marcbrinkmann.de>
2017-08-28 18:30:08 +0200

    Release version 0.4.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name='jsonext',
-    version='0.4.2.dev1',
+    version='0.4.2',
     description='Well-structured helpers to help serializing commonly '
     'encountered structures to JSON (like datetime, to_dict(), '
     ' etc.',
